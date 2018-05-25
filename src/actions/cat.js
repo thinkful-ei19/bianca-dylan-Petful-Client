e64b78bdafca659ae8e6b9eaf1f69bcb1244da35
Dylan Stratton <dm.stratton94@gmail.com>
2018-05-25 11:29:44 -0500

    adopt a cat action

--- src/actions/cat.js
+++ src/actions/cat.js
@@ -31,3 +31,34 @@ export const fetchCat = () => dispatch => {
     .then(data => dispatch(fetchCatSuccess(data)))
     .catch(err => dispatch(fetchCatError(err)));
 };
+
+export const ADOPT_CAT_REQUEST = 'ADOPT_CAT_REQUEST';
+export const adoptCatRequest = () => ({
+  type: ADOPT_CAT_REQUEST
+});
+
+export const ADOPT_CAT_SUCCESS = 'ADOPT_CAT_SUCCESS';
+export const adoptCatSuccess = cat => ({
+  type: ADOPT_CAT_SUCCESS,
+  cat
+});
+
+export const ADOPT_CAT_ERROR = 'ADOPT_CAT_ERROR';
+export const adoptCatError = error => ({
+  type: ADOPT_CAT_ERROR,
+  error
+});
+
+export const adoptCat = () => dispatch => {
+  dispatch(adoptCatRequest());
+  return fetch(`${API_BASE_URL}/cat`, {
+    method: 'DELETE'
+  })
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return dispatch(fetchCat());
+    })
+    .then(err => dispatch(adoptCatError(err)));
+};
---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+import { fetchCat, fetchCatRequest, fetchCatSuccess, fetchCatError, adoptCat, adoptCatRequest, adoptCatSuccess, adoptCatError } from './cat';
+
+export { fetchCat, fetchCatRequest, fetchCatSuccess, fetchCatError, adoptCat, adoptCatRequest, adoptCatSuccess, adoptCatError };
