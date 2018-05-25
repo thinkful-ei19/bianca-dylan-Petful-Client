c6950d98b545be2e63ef87c332ee68473cda1506
biancavee15 <vasquezbmarie@gmail.com>
2018-05-25 12:31:48 -0400

    added to dog actions

--- src/actions/dog.js
+++ src/actions/dog.js
@@ -14,9 +14,18 @@ export const fetchDogError = error => ({
     type: FETCH_DOG_ERROR,
     error
 });
+export const ADOPT_DOG_REQUEST = 'ADOPT_DOG_REQUEST';
+export const adoptDogRequest = () => ({
+    type: ADOPT_DOG_REQUEST
+})
+export const ADOPT_DOG_ERROR = 'ADOPT_DOG_ERROR';
+export const adoptDogError = error => ({
+    type: ADOPT_DOG_ERROR,
+    error
+})
 export const fetchDog = () => dispatch => {
     dispatch(fetchDogRequest());
-    return fetch('http://localhost:8080/dog', {
+    return fetch(`${API_BASE_URL}/dog`, {
         method: 'GET'
     })
         .then(res => {
@@ -24,6 +33,20 @@ export const fetchDog = () => dispatch => {
                 return Promise.reject(res.statusText);
             }
             return res.json();
+        })
+        .then(data => dispatch(fetchDogSuccess(data)))
+        .catch(err => dispatch(fetchDogError(err)));
+}
+export const adoptDog = () => dispatch => {
+    dispatch(adoptDogRequest());
+    return fetch(`${API_BASE_URL}/dog`, {
+        method: 'DELETE'
+    })
+        .then(res => {
+            if(!res.ok){
+                return Promise.reject(res.statusText);
+            }
+            return dispatch(fetchDog());
         })
         .then(data => dispatch(fetchDogSuccess(data)))
         .catch(err => dispatch(fetchDogError(err)));
